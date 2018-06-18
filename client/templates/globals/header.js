a203e23cc93871a9a11611efbf5075e343259924
tieze zhang <e0196638@u.nus.edu>
2018-06-18 10:51:51 +0800

    first commit

--- client/templates/globals/header.js
+++ client/templates/globals/header.js
@@ -1,17 +1,6 @@
 Template.header.helpers({
   reminder() {
-    let now  = new Date(),
-        days = {
-          0: 'Sunday',
-          1: 'Monday',
-          2: 'Tuesday',
-          3: 'Wednesday',
-          4: 'Thursday',
-          5: 'Friday',
-          6: 'Saturday'
-        };
-
-    return `It's ${ days[ now.getDay() ] }, we still own you!`;
+    return `Play DnD online!`;
   }
 });
 
