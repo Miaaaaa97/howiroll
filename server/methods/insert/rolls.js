892f4666ebe87a27a988152141bb3169a11038d7
0blivious <39231750+0blivious@users.noreply.github.com>
2018-07-09 18:44:07 +0800

    Add files via upload

---
+++ server/methods/insert/rolls.js
@@ -0,0 +1,17 @@
+import insertRoll from '../../modules/insert-roll';
+
+Meteor.methods({
+  insertRolls( message ) {
+    check( message, {
+      destination: String,
+      isDirect: Boolean,
+      message: String
+    });
+
+    try {
+      insertRoll( message );
+    } catch ( exception ) {
+      throw new Meteor.Error( '500', `${ exception }` );
+    }
+  }
+});
