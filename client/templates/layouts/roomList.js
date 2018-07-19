6469f382d1bf5fef1561b8bb7fbf4a09e48889e2
tieze zhang <e0196638@u.nus.edu>
2018-07-19 09:02:50 +0800

    写了加密和假的search

--- client/templates/layouts/roomList.js
+++ client/templates/layouts/roomList.js
@@ -5,12 +5,18 @@ Template.roomList.events({
 	'click .room': function() {
 		var roomId = this._id;
 		var roomname = this.name;
+		var public = this.public;
+		var password = this.password;
 		Session.set('selectedRoom', roomId);
 		Session.set('selectedRoomName', roomname);
+		Session.set('selectedpublic', public);
+		Session.set('selectedpw', password);
 	},
 	'click .join': function() {
 		var roomId = Session.get('selectedRoom');
 		var roomname = Session.get('selectedRoomName');
+		var public = Session.get('selectedpublic');
+		var pw = Session.get('selectedpw');
 		var currentUserId = Meteor.userId();
 		var joined = Roomjoin.findOne({roomId: roomId, playerId: currentUserId});
 		var created = Rooms.findOne({_id: roomId, createdBy: currentUserId});
@@ -18,9 +24,17 @@ Template.roomList.events({
 			Bert.alert('You already joined this game!', 'warning');
 		} else if (created) {
 			Bert.alert('You are already the GameMaster!', 'warning');
-		} else {
+		} else if (public == true) {
 		    Meteor.call('joinRoom', roomId, roomname);
 		    Bert.alert('Joined game successfully!', 'success');
+		} else {
+			var password = prompt("This is a private room. Please enter password: ", "password");
+			if (password == pw) {
+				Meteor.call('joinRoom', roomId, roomname);
+		        Bert.alert('Joined game successfully!', 'success');
+			} else {
+				Bert.alert('Wrong password!', 'warning');
+			}
 		}
 	}
 });
