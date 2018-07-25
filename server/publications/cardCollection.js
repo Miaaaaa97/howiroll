2ebca4a93c2adc3814163eee513a1cba0b1dbbb8
0blivious <39231750+0blivious@users.noreply.github.com>
2018-07-25 17:27:36 +0800

    Add files via upload

--- server/publications/cardCollection.js
+++ server/publications/cardCollection.js
@@ -1,3 +1,3 @@
 Meteor.publish( 'getCards', function() {
-	return CharacterCards.find({owner: { $in: ["public", this.userId]}});
+	return CharacterCards.find({owner: { $in: ["public", this.userId]}}, { sort: { createAt: 1 } });
 });
